89277857ea26a74cb477fd7046756f644bf188a3
Nabeelah <nabeelahyousuph@gmail.com>
2019-07-12 10:25:50 +0100

    Create endpoint to get specific action

--- actions/actionsRouter.js
+++ actions/actionsRouter.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const Action = require("../data/helpers/actionModel");
 const router = express.Router();
+const {actionMiddleware}  = require("../middleware");
 
 router.get("/", async (req, res) => {
   try {
@@ -12,5 +13,17 @@ router.get("/", async (req, res) => {
     });
   }
 });
+
+router.get("/:id", actionMiddleware.validateActionId, async (req, res) => {
+    try {
+      const { id } = req.params;
+      const action = await Action.get(id);
+      res.status(200).json(action);
+    } catch (error) {
+      res.status(500).json({
+        message: "Error retrieving action"
+      });
+    }
+  });
 
 module.exports = router;
