fbc4c959bd8be91cfc89018c23988071d683633c
Nabeelah <nabeelahyousuph@gmail.com>
2019-07-12 10:53:34 +0100

    Create endpoint  to edit action

--- actions/actionsRouter.js
+++ actions/actionsRouter.js
@@ -28,13 +28,24 @@ router.get("/:id", actionMiddleware.validateActionId, async (req, res) => {
 
 router.post("/", actionMiddleware.validateAction, async (req, res) => {
   try {
-    const { id } = req.params;
-    // const { description, notes } = req.body;
     const newAction = await Action.insert(req.body);
     res.status(201).json(newAction);
   } catch (error) {
     res.status(500).json({
       message: "Error creating action"
+    });
+  }
+});
+
+router.put("/:id", actionMiddleware.validateActionId, async (req, res) => {
+  try {
+    await Action.update(req.params.id, req.body);
+    const editedAction = await Action.get(req.params.id);
+    res.status(200).json(editedAction);
+  } catch (error) {
+      console.log(error)
+    res.status(500).json({
+      message: "Error editing the action"
     });
   }
 });
