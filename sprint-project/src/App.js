b1197c96759371bd6fb8c8cb6aacd5c44588ce10
Nabeelah <nabeelahyousuph@gmail.com>
2019-07-12 11:37:50 +0100

    Add link to home

--- sprint-project/src/App.js
+++ sprint-project/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./App.css";
 import Projects from "./components/Projects";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import ProjectDetail from "./components/ProjectDetail";
 
 function App() {
   return (
     <Router>
+      <Link to="/">Home</Link>
       <div className="App">
         <Route path="/" exact component={Projects} />
       </div>
